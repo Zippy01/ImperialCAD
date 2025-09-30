031f32d81f3e2d9d60bde9e2115be9fc1e440919
Zippy OFC <72061397+Zippy01@users.noreply.github.com>
2025-09-29 21:07:11 -0400

    Fix typo in version key and comment out duty check in live map event handler

--- plugins/ImperialLiveMap/sv_imperiallm.lua
+++ plugins/ImperialLiveMap/sv_imperiallm.lua
@@ -12,11 +12,11 @@ AddEventHandler("ImperialCAD:livemap:send", function(data)
     local communityId = GetConvar("imperial_community_id", "")
 
     if not discord then return end
-    if not IsUnitOnDuty(src) then print("[Imperial LiveMap] Not Sending data, unit is off duty") TriggerClientEvent('ImperialCAD:livemap:client:ToggleTracking', src, false) return end
+    --if not IsUnitOnDuty(src) then print("[Imperial LiveMap] Not Sending data, unit is off duty") TriggerClientEvent('ImperialCAD:livemap:client:ToggleTracking', src, false) return end
     if Config.debug then print("[Imperial LiveMap] Sending livemap data") end
 
     PerformHttpRequest('https://map.imperial-solutions.net/livemap', function(err, text, headers) end, "POST", json.encode({
-        verison = 2,
+        version = 2,
         cid = communityId,
         discord = discord,
         x = data.x,
